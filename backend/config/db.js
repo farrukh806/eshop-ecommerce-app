57cd760ec6def32a56cb2698a3497246f85c6a68
Farrukh Jamal <farrukhjamal806@gmail.com>
2021-03-29 10:17:42 +0530

    All models added for DB

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+	try {
+		const conn = mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log(
+			`MongoDB is Connected: ${(await conn).connection.host}`.cyan.underline
+		);
+	} catch (error) {
+		console.error(`Error:${error.message}`.red.bold);
+		process.exit(1);
+	}
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
+import colors from 'colors';
+import products from './data/products.js';
+import connectDB from './config/db.js';
 const app = express();
-const products = require('./data/products');
 const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+connectDB();
 
 app.get('/', (req, res) => {
 	res.send('API is running');
@@ -16,6 +22,9 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product);
 });
 app.listen(PORT, (success, err) => {
-	if (!err) console.log(`Server started at ${PORT}`);
+	if (!err)
+		console.log(
+			`Server started at ${PORT} in ${process.env.NODE_ENV} mode`.magenta.bold
+		);
 	else console.log(err);
 });
