fcb9d8405dda31949480000de6cc1c5f34b3821f
Farrukh Jamal <52830299+farrukh806@users.noreply.github.com>
2021-06-18 20:38:31 +0530

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
 	{
 		name: 'Admin',
 		email: 'admin@example.com',
-		password: bcrypt.hashSync('123456', 10),
+		password: bcrypt.hashSync('admin', 10),
 		isAdmin: true,
 	},
 	{
 		name: 'John',
 		email: 'john@example.com',
-		password: bcrypt.hashSync('123456', 10),
+		password: bcrypt.hashSync('john', 10),
 	},
 	{
 		name: 'Jane',
 		email: 'jane@example.com',
-		password: bcrypt.hashSync('123456', 10),
+		password: bcrypt.hashSync('jane', 10),
 	},
 ];
 
