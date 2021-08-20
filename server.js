e882a790df2be9fefa7c514ea0cbbba0b69cfef4
Farrukh <farrukhjamal806@gmail.com>
2021-08-20 17:26:54 +0530

    .env configured

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import path from 'path';
-import dotenv from 'dotenv';
-import express from 'express';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import path from "path";
+import dotenv from "dotenv";
+import express from "express";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const app = express();
 
@@ -17,28 +17,28 @@ app.use(express.json());
 connectDB();
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) =>
-	res.send(process.env.PAYPAL_CLIENT_ID)
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'frontend/build')));
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'))
-	);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"))
+  );
 }
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, (success, err) => {
-	if (!err) {
-		console.log(`Server started at ${PORT}`.magenta);
-	} else console.error(err);
+  if (!err) {
+    console.log(`Server started at ${PORT}`.magenta);
+  } else console.error(err);
 });
